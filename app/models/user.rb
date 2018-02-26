33f69ae5589c430c130ab37dbbba3f2853f86be7
Stephen Carico <stephen.r.carico@gmail.com>
2018-02-25 17:00:10 -0800

    Added create user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
 end
