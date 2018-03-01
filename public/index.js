24ee3aa498d0b11fb3313f2adcefa4c5dc793b67
Stephen Carico <stephen.r.carico@gmail.com>
2018-02-28 19:53:14 -0800

    Created functional signup frontend

---
+++ public/index.js
@@ -0,0 +1,101 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var PostNewPage = {
+  template: "#post-new-page",
+  data: function() {
+    return {
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  created: function() {},
+  methods: {
+    submit: function() {
+      var params = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var LogoutPage = {
+  template: "#logout-page",
+  data: function() {
+    return {
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/newpost", component: PostNewPage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage }
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
