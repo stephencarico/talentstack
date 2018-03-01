845c13c0e32a92b8df0c9e12c72372a09b24f0bc
Stephen Carico <stephen.r.carico@gmail.com>
2018-02-28 21:02:39 -0800

    created user CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
+  # USERS
   post 'user_token' => 'user_token#create'
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+
+  # POSTS
+
+
+  # COMMENTS
+
+
+  # TAGS
+
 
 end
