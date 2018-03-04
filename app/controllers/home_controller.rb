eb4e9ed66145263b533512f61498a43b46350c47
Stephen Carico <stephen.r.carico@gmail.com>
2018-03-04 13:26:52 -0800

    deleted frontend views and v1 files

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    render json: {message: "Hello?"}
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,37 @@ Rails.application.routes.draw do
   resources :posts do 
     resources :comments
   end
+
   resources :tags
   post 'user_token' => 'user_token#create'
   resources :users
-  # QUESTION: can we place users in a v1?
 
 end
+
+# --------------------------- RAILS ROUTES: ------------------------------- 
+# home_index GET    /home/index(.:format)                  home#index
+# post_comments GET    /posts/:post_id/comments(.:format)     comments#index
+#               POST   /posts/:post_id/comments(.:format)     comments#create
+#  post_comment GET    /posts/:post_id/comments/:id(.:format) comments#show
+#               PATCH  /posts/:post_id/comments/:id(.:format) comments#update
+#               PUT    /posts/:post_id/comments/:id(.:format) comments#update
+#               DELETE /posts/:post_id/comments/:id(.:format) comments#destroy
+#         posts GET    /posts(.:format)                       posts#index
+#               POST   /posts(.:format)                       posts#create
+#          post GET    /posts/:id(.:format)                   posts#show
+#               PATCH  /posts/:id(.:format)                   posts#update
+#               PUT    /posts/:id(.:format)                   posts#update
+#               DELETE /posts/:id(.:format)                   posts#destroy
+#          tags GET    /tags(.:format)                        tags#index
+#               POST   /tags(.:format)                        tags#create
+#           tag GET    /tags/:id(.:format)                    tags#show
+#               PATCH  /tags/:id(.:format)                    tags#update
+#               PUT    /tags/:id(.:format)                    tags#update
+#               DELETE /tags/:id(.:format)                    tags#destroy
+#    user_token POST   /user_token(.:format)                  user_token#create
+#         users GET    /users(.:format)                       users#index
+#               POST   /users(.:format)                       users#create
+#          user GET    /users/:id(.:format)                   users#show
+#               PATCH  /users/:id(.:format)                   users#update
+#               PUT    /users/:id(.:format)                   users#update
+#               DELETE /users/:id(.:format)                   users#destroy
