bbd10e95ce692f4bf45a927fa6f5b1f93cc47d90
Stephen Carico <stephen.r.carico@gmail.com>
2018-03-17 22:21:09 -0700

    created controller and validations

---
+++ app/controllers/post_tags_controller.rb
@@ -0,0 +1,15 @@
+class PostTagsController < ApplicationController
+
+  # There will be no user interface to connect posts&tags
+  def create
+    post_tag = PostTag.create(
+      post_id: params[:post_id],
+      tag_id: params[:tag_id]
+    )
+  end
+  # User flow: In posts/create, user will fill form field
+  # that requests to enter tag.
+  # QUESTION: How do we work around the need for post id
+  # after creation?
+
+end
