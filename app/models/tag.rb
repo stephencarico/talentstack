668d7e505a3dea9f1b44f9b348f03defa53f985b
Stephen Carico <stephen.r.carico@gmail.com>
2018-03-18 17:05:59 -0700

    started index tagged posts on show

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,12 @@
 class Tag < ApplicationRecord
   has_many :post_tags
   has_many :posts, through: :post_tags
+
+  # def as_json
+  #   {
+  #     id: id,
+  #     title: title,
+  #     posts: posts.map {|post| post }
+  #   }
+  # end
 end
